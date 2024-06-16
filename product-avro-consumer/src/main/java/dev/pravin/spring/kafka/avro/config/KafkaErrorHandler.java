41e22c3ab373d26671440c6824d1e610eb8f53b9
Pravin Choudhary <Pravin.Choudhary@sainsbury's.co.uk>
2024-06-16 11:09:22 +0100

    handle poison pill by logging the details

---
+++ product-avro-consumer/src/main/java/dev/pravin/spring/kafka/avro/config/KafkaErrorHandler.java
@@ -0,0 +1,26 @@
+package dev.pravin.spring.kafka.avro.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@EnableKafka
+@Configuration
+@Slf4j
+public class KafkaErrorHandler {
+
+    /*
+    SpringBoot will automatically detect this bean and use it as the default error handler for all listeners.
+     */
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+        return new DefaultErrorHandler(
+                (record, exception) -> {
+                    log.info("Error occurred while processing: " + record);
+                    log.warn("Exception: " + exception);
+                }, new FixedBackOff(0L, 2L) );
+    }
+}
